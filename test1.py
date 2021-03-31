285fb7b7402b500e609a04a3691c1f2fd4eb3d5b
Yuuty <ch3ch2ch2ch3jpjp@gmail.com>
2021-03-31 21:34:24 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -189,4 +189,28 @@ print(d)
 
 print('a' in d)
 
+fruits = {
+    'apple' : 100,
+    'banana': 200,
+    'orange': 300,
+}
 
+print(fruits['apple'])
+
+a = {1, 2, 3, 4, 4, 4, 5, 6}
+print(a)
+
+b = {2, 3, 3, 6, 7}
+print(b)
+
+print( b - a )
+
+
+
+my_friends = {'A', 'C', 'D'}
+A_friends = {'B', 'D','E','F'}
+print(my_friends & A_friends)
+
+f = ['apple', 'banana', 'apple']
+kind = set(f)
+print(kind)
