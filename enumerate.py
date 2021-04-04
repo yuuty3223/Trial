a515a68bf09ac3c2f2d9661e33c56c8c20d0c178
Yuuty <ch3ch2ch2ch3jpjp@gmail.com>
2021-04-04 21:47:07 +0900

    test
    test program

---
+++ enumerate.py
@@ -0,0 +1,7 @@
+#i = 1
+#for fruits in ['apple', 'banana', 'orange']:
+#    print(i, fruits)
+#    i += 1
+#
+for i, fruit in enumerate(['apple', 'banana', 'orange']):
+    print(i, fruit)
---
+++ while.py
@@ -0,0 +1,28 @@
+some_list = [1, 2, 3, 4, 5]
+
+#i = 0
+#while i < len(some_list):
+#    print(some_list[i])
+#
+#
+#for i in some_list:
+#    print(i)
+
+#for s in 'abcde':
+#    print(s)
+#
+
+#for word in ['My','Name', 'is', 'Mike']:
+#    if word == 'Name':
+#        continue
+#        #break
+#    print(word)
+
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print('stop eating!')
+        break
+    print(fruit)
+else:
+    print('I ate all!')
+
